89f9a218773913df458b687aa3c04079d3a27e59
ShaikNajima21 <najimashaik2006@gmail.com>
2026-03-13 21:32:25 +0530

    Add Java program to reverse a string

---
+++ Day1_ReverseString.java
@@ -0,0 +1,16 @@
+// Problem: Reverse String
+// Language: Java
+// Author: Shaik Najima
+
+public class Day1_ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+    }
+}
