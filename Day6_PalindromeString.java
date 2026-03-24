0fa33ae2fe0eeb82d9c4cbc1b550e7876b3bd639
ShaikNajima21 <najimashaik2006@gmail.com>
2026-03-24 20:43:42 +0530

    Implement palindrome string checker in Java

---
+++ Day6_PalindromeString.java
@@ -0,0 +1,20 @@
+// Problem: Palindrome String
+// Language: Java
+// Author: Shaik Najima
+
+public class Day6_PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
