1e0079ddbd8813324cb96cdc7c4f6681b4a9fa74
ShaikNajima21 <najimashaik2006@gmail.com>
2026-03-28 22:12:46 +0530

    Implement recursive factorial function in Java

---
+++ Day10_FactorialRecursion.java
@@ -0,0 +1,22 @@
+// Problem: Factorial using Recursion
+// Language: Java
+// Author: Shaik Najima
+
+public class Day10_FactorialRecursion {
+
+    // Recursive function
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+
+        int result = factorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
