9d7db2b062b80a9c80f4a203b36c4dc4237a7b70
ShaikNajima21 <najimashaik2006@gmail.com>
2026-04-02 11:14:44 +0530

    Add Armstrong number checker in Java

---
+++ Day15_ArmstrongNumber.java
@@ -0,0 +1,22 @@
+// Problem: Check Armstrong Number
+// Language: Java
+// Author: Shaik Najima
+
+public class Day15_ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+        while (num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num = num / 10;
+        }
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
+}
