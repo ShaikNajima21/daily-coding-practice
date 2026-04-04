0580716318d7b4b3d9f4a75a76f3071eb7f77b97
ShaikNajima21 <najimashaik2006@gmail.com>
2026-04-04 21:12:03 +0530

    Add Java program to check if a number is even or odd

---
+++ Day17_EvenOdd.java
@@ -0,0 +1,15 @@
+// Problem: Check Even or Odd
+// Language: Java
+// Author: Shaik Najima
+
+public class Day17_EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
