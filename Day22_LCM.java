e093de4140599704ea8ed063b52538850e037488
ShaikNajima21 <najimashaik2006@gmail.com>
2026-04-09 21:06:33 +0530

    Implement LCM calculation in Day22_LCM.java

---
+++ Day22_LCM.java
@@ -0,0 +1,22 @@
+// Problem: Find LCM (Least Common Multiple)
+// Language: Java
+// Author: Shaik Najima
+
+public class Day22_LCM {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 12, b = 18;
+
+        int lcm = (a * b) / gcd(a, b);
+
+        System.out.println("LCM is: " + lcm);
+    }
+}
