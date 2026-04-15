cf58cfbcbc04fc8aa06c397d98b5ef382c328e1e
ShaikNajima21 <najimashaik2006@gmail.com>
2026-04-15 21:59:00 +0530

    Add Java program to check for perfect numbers

---
+++ Day23_PerfectNumber.java
@@ -0,0 +1,21 @@
+// Problem: Check Perfect Number
+// Language: Java
+// Author: Shaik Najima
+
+public class Day23_PerfectNumber {
+    public static void main(String[] args) {
+        int num = 28;
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is a Perfect Number");
+        } else {
+            System.out.println(num + " is not a Perfect Number");
+        }
+    }
+}
